def66c3162ae2428ccb9c3bf24ad2ed49917d55c
Pedro Sotero <pedro.almeida@oi.net.br>
2018-01-03 19:18:23 -0200

    fallback para development build

--- scripts/start.js
+++ scripts/start.js
@@ -3,6 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
+process.env.REACT_APP_ENV = process.env.REACT_APP_ENV || 'development';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -77,8 +78,8 @@ choosePort(HOST, DEFAULT_PORT)
       openBrowser(urls.localUrlForBrowser);
     });
 
-    ['SIGINT', 'SIGTERM'].forEach(function(sig) {
-      process.on(sig, function() {
+    ['SIGINT', 'SIGTERM'].forEach(function (sig) {
+      process.on(sig, function () {
         devServer.close();
         process.exit();
       });
